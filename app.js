2a067ee65daff5aaa080e5ba17b44e0150099575
Juliane Blier <blier.juliane@gmail.com>
2016-09-30 12:35:33 +0200

    Création du cookieClicker et des messages d'alerte

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 $(document).ready(function(){
-	//votre code
+
+	var count = 0;
+
+	$('img').click(function(){
+		count++;
+		console.log(count);
+		$('span').text(count);
+
+		if(count % 60 === 0){
+			alert('MAIS DEGAAAAAAGE');
+		}
+		else if (count % 40 === 0) {
+			alert('Sur que tu veux pas faire autre chose ?')
+		}
+		else if (count %20 === 0) {
+			alert('Allez, va faire un truc intéressant.')
+		}
+	});
 
 
 });
